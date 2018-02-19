24f7f73a6cb1818a574b9eda1f00a475c2109936
Artem <weirdGuy@users.noreply.github.com>
2018-02-19 19:08:33 +0200

    Buttons and integrations

--- index.php
+++ index.php
@@ -126,7 +126,7 @@
                     <div class="menu">
                         <ul>
                             <li><a href="#hiw">Hur det fungerar</a></li>
-                            <li><a href="#">FAQ</a></li>
+                            <li><a href="http://help.kandio.se/">FAQ</a></li>
                             <li><a href="#">Karriär</a></li>
                             <li><a href="#">Contact</a></li>
                         </ul>
@@ -504,6 +504,11 @@
     </div>
 
 </div>
-
+<script>
+  window.intercomSettings = {
+    app_id: "tzqxa7nv"
+  };
+</script>
+<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/tzqxa7nv';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
 </body>
 </html>
